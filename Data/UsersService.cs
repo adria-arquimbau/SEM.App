6f05c9e48f4fb4caa1c46e94e418b68ea0bc9246
Adria Arquimbau <adria.arquimbau@gmail.com>
2022-11-01 22:34:39 +0100

    feat: add users in admin panel page

---
+++ Data/UsersService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace SEM.App.Data;
+
+public class UsersService
+{
+    private readonly HttpClient _httpClient;
+    private const string semApiUrl = "https://sport-management-api.azurewebsites.net/api/";
+
+    public UsersService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<List<UserDto>> GetUsers(string token)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, semApiUrl +  "User");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var response = await _httpClient.SendAsync(request);
+        
+        return await response.Content.ReadFromJsonAsync<List<UserDto>>();
+    }
+}
+        
+public class UserDto
+{   
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime? LastLoginTime { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public bool EmailConfirmed { get; set; }
+    public DateTime? RegistrationDate { get; set; }
+}  
--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SEM.App;
 using SEM.App.Data;
 using SEM.App.Utilities;
-using SportEventManager.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton<HttpClient>();
 builder.Services.AddSingleton<AuthService>();
 builder.Services.AddSingleton<EventsService>();
+builder.Services.AddSingleton<UsersService>();
 builder.Services.AddScoped<ILocalStorage, LocalStorage>();
 
 await builder.Build().RunAsync();
