c7ada9d9ba9cbb734a33aa760498216400f4c02d
Adria Arquimbau <adria.arquimbau@gmail.com>
2022-11-04 15:57:50 +0100

    feat: user service

--- Data/UsersService.cs
+++ Data/UsersService.cs
@@ -32,9 +32,18 @@ public class UsersService
 
         return response.IsSuccessStatusCode;
     }
+
+    public async Task<UserDto> GetMyUser(string token)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, semApiUrl +  "User/my-user");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var response = await _httpClient.SendAsync(request);
+        
+        return await response.Content.ReadFromJsonAsync<UserDto>();
+    }   
 }
         
-public class UserDto
+public class UserDto    
 {   
     public Guid Id { get; set; }
     public string Name { get; set; }
@@ -43,4 +52,10 @@ public class UserDto
     public string Email { get; set; }
     public bool EmailConfirmed { get; set; }
     public DateTime? RegistrationDate { get; set; }
+    public string SecondName { get; set; }
+    public string Phone { get; set; }
+    public string PostalCode { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
 }  
