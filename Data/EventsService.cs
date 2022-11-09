b3e5f771fc10cae2dbf9295313789ae506e77a90
Adria Arquimbau <adria.arquimbau@gmail.com>
2022-11-09 22:18:06 +0100

    feat: I can manage this event only for event creators

--- Data/EventsService.cs
+++ Data/EventsService.cs
@@ -145,8 +145,9 @@ public class RegisterToAnEventResponse
 public class SportEvent 
 {   
     public Guid Id { get; set; }
+    public Guid CreatorUserId { get; set; }
     public string Name { get; set; }    
-    public string Description { get; set; }
+    public string Description { get; set; } 
     public string CreatorNickName { get; set; }
     public DateTime CreationDate { get; set; }
     public int RegistrationsQuantity { get; set; }
