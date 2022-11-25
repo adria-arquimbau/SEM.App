1c863e5d3dba16233ac2608bfd3fa3bb3cb96c1b
Adria Arquimbau <adria.arquimbau@gmail.com>
2022-11-25 11:02:28 +0100

    feat:getShort description

--- SEMApp/Data/Models/GetNewsDto.cs
+++ SEMApp/Data/Models/GetNewsDto.cs
@@ -4,6 +4,7 @@ public class GetNewsDto
 {
     public Guid Id { get; set; }
     public string Description { get; set; }
+    public string ShortDescription { get; set; }
     public Guid EventId { get; set; }
     public string Title { get; set; }
     public DateTime CreationDate { get; set; }
