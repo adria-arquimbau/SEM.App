1deb7532262f945ab8310d9b3e704529b0ea4659
Adria Arquimbau <a.arquimbau@palfinger.com>
2022-11-28 14:46:41 +0100

    fix: loading button

--- SEMApp/Authentication/RoleConstants.cs
+++ SEMApp/Authentication/RoleConstants.cs
@@ -1,9 +1,7 @@
-﻿namespace SEM.App.Authentication
+﻿namespace SEM.App.Authentication;
+public static class RoleConstants
 {
-    public static class RoleConstants
-    {
-        public static string User => "SEM1:SPORT_EVENTS_MANAGEMENT_USER";
-        public static string Organizer => "SEM2:SPORT_EVENTS_MANAGEMENT_ORGANIZER";
-        public static string Admin => "SEM3:SPORT_EVENTS_MANAGEMENT_ADMIN";
-    }
-}       
+    public static string User => "SEM1:SPORT_EVENTS_MANAGEMENT_USER";
+    public static string Organizer => "SEM2:SPORT_EVENTS_MANAGEMENT_ORGANIZER";
+    public static string Admin => "SEM3:SPORT_EVENTS_MANAGEMENT_ADMIN";
+}
