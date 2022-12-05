a48aa3c920dca146e7f66df5577328f325aeb48d
Adria Arquimbau <a.arquimbau@palfinger.com>
2022-12-05 15:12:23 +0100

    feat: schedule url

---
+++ SEMApp/Data/Models/ScheduleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SEM.App.Data.Models;
+
+public class ScheduleDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DateTime { get; set; }
+}
