8c4a5325c7d20fdba7b2416432dc04df5e4540c2
Adria Arquimbau <adria.arquimbau@gmail.com>
2022-12-14 00:37:34 +0100

    feat: managers manager

---
+++ SEMApp/Data/Models/ManagersDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SEM.App.Data.Models;
+
+public class ManagersDto
+{
+    public Guid Id { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public ManagerRole Role { get; set; }
+    public UserDto User { get; set; }
+}
+
+public enum ManagerRole
+{
+    Basic,
+    CheckIn,
+    Admin,
+    Owner
+}
