caee8fccc51a362d83449df0c1dcf08bd3b4f86d
adria-arquimbau <aarquimbau@voxelgroup.net>
2022-12-15 18:35:32 +0100

    refact: error api response

---
+++ SEMApp/Shared/Messages.cs
@@ -0,0 +1,6 @@
+namespace SEM.App.Shared;
+
+public static class Messages
+{
+    public static readonly string ApiInternalServerErrorMessage = "Something went wrong while fetching the data, try again later.";
+}
